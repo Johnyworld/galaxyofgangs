f3abe340c45d068318f9983035d1868ab662c3ef
johnyworld <johnyworld@naver.com>
2021-02-22 09:16:36 +0900

    Refactor to class

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,13 +1,45 @@
-
-const socket = io('http://localhost:7000', { autoConnect: false });
-
-socket.connect();
-
-socket.emit('hello', () => {
-  console.log('connection?')
-});
-
 let title = <HTMLElement> document.getElementById('title');
 title.style.color = 'violet';
 title.innerText = 'Hello Galaxy!'
+
+class Socket {
+  socket: SocketIOClient.Socket;
+  constructor() {
+    this.socket = io('http://localhost:7000', { autoConnect: false });
+    this.connect();
+  }
+
+  connect() {
+    this.socket.connect();
+  }
+
+  hello() {
+    this.socket.emit('hello', () => {
+      console.log('connection?')
+    });
+  }
+}
+
+
+class App {
+  socket: Socket;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  constructor() {
+    this.socket = new Socket();
+    this.socket.hello();
+    this.canvas = <HTMLCanvasElement> document.getElementById('canvas');
+    this.ctx = <CanvasRenderingContext2D> this.canvas.getContext('2d');
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(50, 50, 50, 50);
+  }
+
+  draw() {
+  }
+}
+
+
+window.onload = () => {
+  new App();
+}
 
